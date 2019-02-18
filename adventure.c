ec243e81d2a691b475186b2941567c12949e2d74
Veronica Gusev <cpgusev@gmail.com>
2019-02-18 16:06:04 -0500

    implemented input and set up rooms

--- adventure.c
+++ adventure.c
@@ -1,32 +1,33 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "items.h"
 #include "rooms.h"
 #include <string.h>
-#include<stdbool.h>
+#include <stdbool.h>
 
 
-//#define 
-
-struct avatar{
- char*name;
- struct Item*bag;
- int score;
- chamber*stage;
+struct user{
+ struct Item* bag;
+ chamber* stage;
 };
-typedef struct avatar user;
 
-struct avatar* initialize(char*name,struct Item*bag, chamber* begin){
-	struct avatar*user=(struct avatar*)malloc(sizeof(struct avatar));
-	user->name=name;
-	user->bag=bag;
-	user->stage=begin;
-	user->score=0;
-	return user;
+typedef struct user user;
+
+
+
+ user* initialize(struct Item*bag, chamber* begin){
+	struct user* mario=(struct user*)malloc(sizeof(struct user));
+	mario->bag=bag;
+	mario->stage=begin;
+	return mario;
 }
 
 void add_item(struct Item*bag, struct Item*new){ //pass a pointer to add the item
-	if(bag->next==NULL){
+
+	if(bag == NULL){
+		bag = new;
+	}
+	else if(bag->next==NULL){
 		bag->next=new;
 	}	
 	while(bag->next!=NULL){
@@ -37,97 +38,108 @@ void add_item(struct Item*bag, struct Item*new){ //pass a pointer to add the ite
 }
 
 
-void LOOK_cmd(chamber*ptr){
+void look(chamber*ptr){
 	display_room(ptr);
 }
 
 
-void interact_room(){
-
-	
-}
 
 
-
-
-bool room_exit(user*ptr , char* direction){ 
+bool go(user* ptr, char* direction){ 
 	chamber*temp=ptr->stage;
 
-	if(strcmp("north",direction)==0 && (temp->north->challenge)){
+	if(strcmp("NORTH",direction)==0 && (temp->north->challenge)){
   	ptr->stage=temp->north;
   	return true;
    	}
-  	if(strcmp("south",direction)==0 && (temp->south->challenge)){
+  	if(strcmp("SOUTH",direction)==0 && (temp->south->challenge)){
   	ptr->stage=temp->south;
   	return true;
   	}
-  	else if(strcmp("east",direction)==0 && (temp->east->challenge)){
+  	else if(strcmp("EAST",direction)==0 && (temp->east->challenge)){
   	ptr->stage=temp->east;
   	return true;
   	}
-  	else if(strcmp("west",direction)==0 && (temp->west->challenge)){
+  	else if(strcmp("WEST",direction)==0 && (temp->west->challenge)){
   	ptr->stage=temp->west;
   	return true;
   	}
-  	else if(strcmp("up",direction)==0 && (temp->up->challenge)){
+  	else if(strcmp("UP",direction)==0 && (temp->up->challenge)){
   	ptr->stage=temp->up;
   	return true;
   	}
- 	else if(strcmp("down",direction)==0 && (temp->down->challenge)){
+ 	else if(strcmp("DOWN",direction)==0 && (temp->down->challenge)){
   	ptr->stage=temp->down;
   	return true;
   	}
   	else
-  	printf("The wey is blocked");
+  	printf("You can't go that way.");
 	free(temp);
 	return true;
 }
 
 
-void play_game(user*ptr){ //USER API
-	char decision;
-	char direction;
-	bool stop=false;
-	while(!stop){
-		printf("CHOOSE AN ACTION: L For LOOK  T to TAKE , G to go or Q to quit or I for INVENTORY:");
-		scanf(" %c",&decision);
-		if(decision=='L'){
-			LOOK_cmd(ptr->stage);
-			continue;
-		}
-		else if(decision='I'){
-			print_list(ptr->bag);
-			continue;
-		}
-		else if(decision=='Q'){
-			break;
-	}
+void play_game(user* ptr){ //USER API
+	char* input1 = (char*)malloc(sizeof(char*));
+	char* input2 = (char*)malloc(sizeof(char*));
 
-printf("Thank for playing");
+	char direction;
+	bool end=false;
+
+	while(!end){
+
+		printf("What do you do? Type \"help\" for a list of commands\n");
+
+		scanf("%s",input1);
+
+
+		if(strcmp("look", input1) == 0){
+			look(ptr->stage);
+		}
+		else if(strcmp("go", input1) == 0){
+					scanf("%s",input2);
+			go(ptr,input2);
+		}
+		else if(strcmp("take", input1) == 0){
+					scanf("%s",input2);
+
+		}
+		else if(strcmp("use", input1) == 0){
+					scanf("%s",input2);
+
+		}
+		else if(strcmp("drop", input1) == 0){
+					scanf("%s",input2);
+
+		}
+		else if(strcmp("help", input1) == 0){
+			printf("Type \"go\" to go to another room. Type \"look\" to look at your surroundings.\nType \"take\" to take an item in a room. Type \"drop\" to drop something from your inventory.\nType \"use\" to use something in your inventory. Type \"quit\" to quit the gane. Your progress won't be saved.");
+		}
+		else if(strcmp("quit", input1) == 0){
+			end = true;
+		}
+		else{
+			printf("I don't know what you mean.\n");
+		}
 	}
+	
 }
 
-typedef struct avatar user;
 
-int main(void){
-char*name;
-struct Item *bag=item("","",item("ID","To enter club",NULL));
-chamber*first_stage=initiate_room();
-printf("Welcome to Text Adventure\nPlease choose a name for you avatar:");
-scanf("%s",name);
-struct avatar*user=initialize(name,bag,first_stage);
-printf("%s\n",user->name);
-play_game(user);
+int main(){
+	//prototypes
+	chamber* load_rooms();
 
-/*room_item(first_stage);
-print_list(bag);
-printf("*PLAYER INVENTORY*\n");
-add_item(bag,item_take("key",first_stage->item));
-//printf("%s",(item_take("key",first_stage->item))->description);
-print_list(bag);
 
-printf("first stage after remove\n");
+	//initializes user with a NULL inventory and load_rooms() returning the starting room
+	chamber* start = load_rooms();
+	user* mario = initialize(NULL, start);
 
-display_room(first_stage);*/
-return 0;
+
+	printf("\n\nAnother perfect day at the Mushroom Kingdom has been ruined yet again. Princess Toadstool has been kidnapped and taken away to a far off castle.\nNow it's your job to get her back. Your story starts as you travel to the looming castle and opening its metal doors. They shut loudly behind you.\n\n");
+	
+
+	play_game(mario);
+
+	return 0;
 }
--- rooms.c
+++ rooms.c
@@ -27,47 +27,37 @@ chamber * new_room(char* desc, char*tool, Item *item_list, chamber *north, chamb
 	return new_room;
 }
 
-void set_north(chamber*current,chamber*other){
+void set_north(chamber* current,chamber* other){
 	current->north=other;
 }
 
-void set_south(chamber*current,chamber*other){
+void set_south(chamber* current,chamber* other){
 	current->south=other;
 }
 
-void set_east(chamber*current,chamber*other){
+void set_east(chamber* current,chamber* other){
 	current->east=other;
 }
 
-void set_west(chamber*current,chamber*other){
+void set_west(chamber* current,chamber* other){
 	current->west=other;
 }
 
-void set_up(chamber*current,chamber*other){
+void set_up(chamber* current,chamber* other){
 	current->up=other;
 }
 
-void set_down(chamber*current,chamber*other){
+void set_down(chamber* current,chamber* other){
 	current->down=other;
 }
 
 
-void display_room(chamber*ptr){
-	printf("YOUR SURROUNDING IS\n");
-	printf("The south is:%s\n",(ptr->south!=NULL? ptr->south->desc:"DEAD END"));
-	printf("The north is:%s\n",(ptr->north!=NULL? ptr->north->desc:"DEAD END"));
-	printf("The east is:%s\n",(ptr->east!=NULL? ptr->east->desc:"DEAD END"));
-	printf("The west is:%s\n",(ptr->west!=NULL? ptr->west->desc:"DEAD END"));
-	printf("Upper chamber is:%s\n",(ptr->up!=NULL? ptr->up->desc:"DEAD END"));
-	printf("Below is:%s\n",(ptr->down!=NULL? ptr->down->desc:"DEAD END")); 
-}
-
-void room_item(chamber*room){
- 	print_list(room->item);
+void display_room(chamber* ptr){
+	printf("%s",ptr->desc);
 }
 
 
-bool state_conquered(char*item,chamber* current){
+bool state_conquered(char* item,chamber* current){
 	if(strcmp(item,current->item_req)==0){
 		current->challenge=true;
 		current->desc="Room mission has been completed";
@@ -77,40 +67,56 @@ bool state_conquered(char*item,chamber* current){
 }
 
 
-chamber* initiate_room(){
+chamber* load_rooms(){
 
-	Item* room_one=item(" "," ",item("sword of ALL Sword","to slay a beast",item("fire","to melt the ice",NULL)));
-	Item* room_two=item(" "," ",item("key","to unlock a chest",NULL));
+	chamber* room_11 = new_room("ROOM 1-1. The room is dim with no lighting except light pouring from the outside of the front gate you've entered from. The stone brick walls surrounds you. You see a rectangular opening EAST of you.",
+	NULL, NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+	chamber* room_12 = new_room("ROOM 1-2. The room's walls are smokey and dark. Metal sconces are chained to the walls and their flames lick up at crumbling ceiling. One of the flames behaves differently from the others and doesn't burn you as you approach. On further inspection, it isn't real fire but a Fire Flower emitting a shining light. An old wooden door NORTH of you blocks your way NORTH. It does not budge. You see a way WEST of you.",
+	"Fire Flower", NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+	chamber* room_13 = new_room("ROOM 1-3. The first thing you notice are stones and dust falling from the rooms ceiling. The ceiling in this room is falling apart and would most likely give if you even touched it, but you are too short as you are. The room's floor gives way to dirt instead of stone. You see a way SOUTH and WEST of you.",
+	"Mushroom", NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+	chamber* room_14 = new_room("ROOM 1-4. Intricate stone stairs spiral up into the ceiling. Small specks of dust and dirt float down from the floor above. You can climb the stairs UP and you see a way EAST of you.",
+	NULL, NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+	chamber* room_21 = new_room("ROOM 2-1. The floor is littered with discarded koopa shells and their putrid smell makes your eyes water. Some of the koopa shells still have skelettons inside them while other skeletons are chained to the wall. You can't help but feel bad. There are stairs that lead DOWN and a way to continue SOUTH of you.",
+	NULL, NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+	chamber* room_22 = new_room("ROOM 2-2. The room is empty except for crumbled and ripped up paper all over the floor. No writing desk is in the room. You pick up a paper and inspect it, but it is indecipherable plans. The poorly drawn plumber and dinosaur remind you of someone. You see a way NORTH and EAST.",
+	NULL, NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+	chamber* room_23 = new_room("ROOM 2-3. Blocks cover the ground and there is suspicious mound of them in the corner of the room. The blocks cannot be lifted and there's no way to break them with a Mushroom since you can't get under them. The room is a dead-end. You see only the way you came from WEST of you.",
+	"Koopa Shell", NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+	chamber* room_24 = new_room("ROOM 2-4. The room is huge and completely black. You're standing on the edge of a bridge that is chained above a lake of lava. A huge green lizard with a spiked shell looms on the other side of the bridge. Fire pours from his unhinged mouth. Behind him is an axe as well as where the bridge is precariously chained.",
+	"Axe", NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+	set_east(room_11,room_12);
+
+	set_north(room_12,room_13);
+	set_west(room_12,room_11);
+
+	set_south(room_13,room_12);
+	set_up(room_13,room_24);
+	set_west(room_13,room_14);
+
+	set_up(room_14,room_21);
+	set_west(room_14,room_13);
+
+	set_south(room_21,room_22);
+	set_down(room_21,room_14);
+
+	set_north(room_22,room_21);
+	set_east(room_22,room_23);
+
+	set_west(room_23,room_22);
+
+
+	return room_11;
 	
-	chamber * level_two=new_room("Then exit is blocked by a boulder, need shroom to break","Shroom",
-	room_one,NULL,NULL,NULL,NULL,NULL,NULL);
 
-	chamber * level_one=new_room("A room that hibernate by a giant serpent","sword",
-	room_two,level_two,NULL,NULL,NULL,NULL,NULL);
-
-	/*chamber * level_three=new_room("This room guarded by army of Koopa","fire ball",
-	room_two,level_two,NULL,NULL,NULL,NULL,NULL);
-
-	chamber * level_four=new_room("This room has a piranhaplant guarding","fire ball",
-	room_two,level_two,NULL,NULL,NULL,NULL,NULL);
-
-	chamber * level_five=new_room("This room has no obstacle","free to loot",
-	room_two,level_two,NULL,NULL,NULL,NULL,NULL);
-
-	chamber * level_six=new_room("A room that hibernate by a giant serpent that can only be killed with the sword of ALL Sword",
-	room_two,level_two,NULL,NULL,NULL,NULL,NULL);
-
-
-	chamber * level_seven=new_room("A room that hibernate by a giant serpent that can only be killed with the sword of ALL Sword",
-	room_two,level_two,NULL,NULL,NULL,NULL,NULL);
-
-
-	chamber * level_eight=new_room("A room that hibernate by a giant serpent that can only be killed with the sword of ALL Sword",
-	room_two,level_two,NULL,NULL,NULL,NULL,NULL);*/
-
-
-
-	return level_one;
 }
 
 
