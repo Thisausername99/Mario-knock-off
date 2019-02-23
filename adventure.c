e01a7e2132e3ba96a848b5af1f4e6752cc78dbfb
Thisausername99 <cody_hoang99@yahoo.com>
2019-02-23 17:56:41 -0500

    Update adventure.c
    minor changes

--- adventure.c
+++ adventure.c
@@ -58,38 +58,36 @@ bool can_access(char*direction, user*ptr){
 
 
 void go(user* ptr, char* direction){ 
-	//chamber*temp=ptr->stage;
-	if(strcmp("NORTH",direction)==0){
+	if(strcasecmp("NORTH",direction)==0){
   		ptr->stage=ptr->stage->north;
   		printf("You moved NORTH\n\n");
   		
    	}
-  	else if(strcmp("SOUTH",direction)==0){
+  	else if(strcasecmp("SOUTH",direction)==0){
   		ptr->stage=ptr->stage->south;
   		printf("You moved SOUTH\n\n");
   		
   	}
-  	else if(strcmp("EAST",direction)==0){
+  	else if(strcasecmp("EAST",direction)==0){
   		ptr->stage=ptr->stage->east;
   		printf("You moved EAST\n\n");
   		
   	}
-  	else if(strcmp("WEST",direction)==0){
+  	else if(strcasecmp("WEST",direction)==0){
   		ptr->stage=ptr->stage->west;
   		printf("You moved WEST\n\n");
   		
   	}
-  	else if(strcmp("UP",direction)==0){
+  	else if(strcasecmp("UP",direction)==0){
   		ptr->stage=ptr->stage->up;
   		printf("You moved UP\n\n");
   		
   	}
- 	else if(strcmp("DOWN",direction)==0){
+ 	else if(strcasecmp("DOWN",direction)==0){
   		ptr->stage=ptr->stage->down;
   		printf("You moved DOWN\n\n");
   		
   	}
-  	//free(temp);
 	
 }
 
@@ -102,11 +100,10 @@ void play_game(user* ptr){ //USER API
 	bool end=false;
 
 	while(!end){
-
 		printf("What do you do? Type \"help\" for a list of commands\n");
 		scanf("%s",input1);
 		
-		if(strcmp("look", input1) == 0){
+		if(strcasecmp("look", input1) == 0){
 			look(ptr->stage);
 		}
 		else if(strcmp("go", input1) == 0){
@@ -115,37 +112,62 @@ void play_game(user* ptr){ //USER API
 				go(ptr,input2);
 			}
 			else
+			system("setterm -bold on");
 			printf("You can't go that way.\n");
 		}
 
 		else if(strcmp("take", input1) == 0){
-			scanf("%s",input2);
-			add_item(ptr->bag, item_take(input2, ptr->stage->item_list));
-			printf("You took add %s",ptr->stage->item->name);
+			printf("THE ITEMS AVAILABLE IN THIS ROOM\n");
+			if(ptr->stage->item->next!=NULL){ // check if room has item
+				print_list(ptr->stage->item);
+				printf("what you want to take:");
+				fgets(input2,100,stdin);
+
+				while(!contain(input2,ptr->stage->item) && !strcasecmp(input2,"x")==0){
+					printf("ITEM IS NOT EXIST!!!!\n");
+					print_list(ptr->stage->item);
+					printf("Choose again or x to stop:");
+					fgets(input2,100,stdin);
+				}
+				add_item(ptr->bag,item_take(input2,ptr->stage->item)); //add item into bag
+				printf("YOUR NEW INVENTORY\n");
+				print_list(ptr->bag);
+				continue;
+				}
+			else
+				printf("ROOM IS EMPTY\n");
+				continue;
 		}
 
 		else if(strcmp("use", input1) == 0){
+			print_list(ptr->bag);
+			printf("WHAT YOU WANT TO USE:\n");
 			scanf("%s",input2);
 			if(strcmp(item_take(input2,ptr->bag)->name,ptr->stage->reqItem)==0){
-			toggleBlocked(ptr->stage);
+				toggleBlocked(ptr->stage);
 			}
 			else
-			printf("You can't use that item here.\n");
+				printf("You can't use that item here.\n");
+				continue;
+		}
+
+		else if(strcmp("drop", input1) == 0){		
+			print_list(ptr->bag);
+			printf("what do you want to drop:"); 
+			scanf("%s",input2);
+			Item*drop_ptr=item_take(input2,ptr->bag);
+			printf("You dropped the %s\n",drop_ptr->name);
+			add_item(ptr->stage->item,drop_ptr); // add item to room item ==drop in room
+			printf("YOUR INVENTORY:\n");
+			print_list(ptr->bag);
 			continue;
 		}
 
-		else if(strcmp("drop", input1) == 0){
-			scanf("%s",input2);
-			Item*drop_ptr=item_take(input2,ptr->bag);
-			printf("You dropped the %s",drop_ptr->name);
-			free(drop_ptr);
-		}
-
-		else if(strcmp("help", input1) == 0){
+		else if(strcasecmp("help", input1) == 0){
 			printf("Type \"go\" to go to another room\nType \"look\" to look at your surroundings.\nType \"take\" to take an item in a room\nType \"drop\" to drop something from your inventory.\nType \"use\" to use something in your inventory\nType \"quit\" to quit the gane. Your progress won't be saved.\n");
 			continue;
 		}
-		else if(strcmp("quit", input1) == 0){
+		else if(strcasecmp("quit", input1) == 0){
 			end = true;
 		}
 		else{
@@ -160,17 +182,15 @@ void play_game(user* ptr){ //USER API
 int main(){
 	//prototypes
 	chamber* load_rooms();
-	Item*bag=item("","",NULL);
-
+	Item*bag=item("","", item("test","testing",NULL));
 	//initializes user with a NULL inventory and load_rooms() returning the starting room
 	chamber* start = load_rooms();
 	user* mario = initialize(bag, start);
 
-
-	printf("\n\nAnother perfect day at the Mushroom Kingdom has been ruined yet again. Princess Toadstool has been kidnapped and taken away to a far off castle.\nNow it's your job to get her back. Your story starts as you travel to the looming castle and opening its metal doors. They shut loudly behind you.\n\n");
+	printf("WORLD DESCRIPTION:\nAnother perfect day at the Mushroom Kingdom has been ruined yet again. Princess Toadstool has been kidnapped and taken away to a far off castle.\nNow it's your job to get her back. Your story starts as you travel to the looming castle and opening its metal doors. They shut loudly behind you.\n\n");
 	
 
 	play_game(mario);
-
+	
 	return 0;
 }
