2d21938fff69539c869b180a867a4b933d3692c4
nhanfiction <47795547+nhanfiction@users.noreply.github.com>
2019-02-24 12:48:22 -0500

    comments and spacing

--- rooms.c
+++ rooms.c
@@ -5,36 +5,33 @@
 #include <stdbool.h>
 
 
-
 chamber * new_room(char* desc, char* tool, Item *item_list, bool blocked, chamber *north, chamber *south,
-	chamber *east, chamber *west, chamber *up, chamber *down){
+	chamber *east, chamber *west, chamber *up, chamber *down){ //creates new room
 
-	chamber*new_room=(chamber*)malloc(sizeof(chamber));
+	chamber*new_room = (chamber*)malloc(sizeof(chamber)); //allocates memory for the room
 		
-	new_room->desc=desc;
-	new_room->reqItem=tool;
-	new_room->item=item_list;
-	new_room->isBlocked= blocked;
-	
+	new_room->desc = desc; //sets description
+	new_room->reqItem = tool; //sets item required to get to the room
+	new_room->item = item_list; //sets item list
+	new_room->isBlocked = blocked; //sets true or false if the room is blocked by something
 
-	new_room->south=south;
-	new_room->north=north;
-	new_room->east=east;
-	new_room->west=west;
-	new_room->up=up;
-	new_room->down=down;
+	new_room->south = south; //sets room to the south
+	new_room->north = north; //sets room to the north
+	new_room->east = east; //sets room to the east
+	new_room->west = west; //sets room to the west
+	new_room->up = up; //sets room up
+	new_room->down = down; //sets room down
 
 	return new_room;
 }
 
-
-bool contain(char*search,Item*curr_room){
-	curr_room=curr_room->next;
-	while(curr_room!=NULL){
-		if(strcmp(search , curr_room->name)==0){
+bool contain(char*search, Item*curr_room){ //checks whether or not item is in room
+	curr_room = curr_room->next; //skips summy head
+	while(curr_room != NULL){
+		if(strcmp(search , curr_room->name)==0){ //item found
 			return true;
 		}
-		curr_room=curr_room->next;
+		curr_room = curr_room->next; //checks next item in the list
 	}
 	return false;
 }
@@ -42,118 +39,89 @@ bool contain(char*search,Item*curr_room){
 
 
 
-void set_north(chamber* current,chamber* other){
-	current->north=other;
+void set_north(chamber* current,chamber* other){ //sets the room north of current room
+	current->north = other;
 }
 
-void set_south(chamber* current,chamber* other){
-	current->south=other;
+void set_south(chamber* current,chamber* other){ //sets the room south of current room
+	current->south = other;
 }
 
-void set_east(chamber* current,chamber* other){
-	current->east=other;
+void set_east(chamber* current,chamber* other){ //sets the room east of current room
+	current->east = other;
 }
 
-void set_west(chamber* current,chamber* other){
-	current->west=other;
+void set_west(chamber* current,chamber* other){ //sets the room west of current room
+	current->west = other;
 }
 
-void set_up(chamber* current,chamber* other){
-	current->up=other;
+void set_up(chamber* current,chamber* other){ //sets the room above the current room
+	current->up = other;
 }
 
-void set_down(chamber* current,chamber* other){
-	current->down=other;
+void set_down(chamber* current,chamber* other){ //sets the room below the current room
+	current->down = other;
 }
 
-void toggleBlocked(chamber* current){
-	current->isBlocked=!(current->isBlocked);
-}
-
-
-void display_room(chamber* ptr){
-	printf("%s",ptr->desc);
+void toggleBlocked(chamber* current){ //changes the blocked status of the room
+	current->isBlocked =! (current->isBlocked);
 }
 
 
+void display_room(chamber* ptr){ //prints the description of the room
+	printf("%s", ptr->desc);
+}
 
-chamber* load_rooms(){
-	Item*item_12 = item("","",item("fireflower", "a Fire Flower", NULL));
 
-	Item*item_11 = item("","",item("mushroom", "TO ENLARGE", NULL));
 
-	/*Item*item_12 = item("","",item("Fire Flower", "a Fire Flower", NULL));
+chamber* load_rooms(){ //creates all the rooms and connects them
+	Item*item_12 = item("","", item("fireflower", "a Fire Flower", NULL)); //creates fire flower for room 1-2
 
-	Item*item_12 = item("","",item("Fire Flower", "a Fire Flower", NULL));*/
-
+	Item*item_11 = item("","", item("mushroom", "TO ENLARGE", NULL)); //creates mushroom for room 1-1
 
 	chamber* room_11 = new_room("ROOM 1-1. The room is dim with no lighting except light pouring from the outside of the front gate you've entered from. The stone brick walls surrounds you.\nYou see a rectangular opening EAST of you.\n",
-
-	NULL,NULL,false,NULL,NULL,NULL,NULL,NULL,NULL);
-
-	chamber* room_12 = new_room("ROOM 1-2. The room's walls are smokey and dark. Metal sconces are chained to the walls and their flames lick up at crumbling ceiling.\nOne of the flames behaves differently from the others and doesn't burn you as you approach.\nOn further inspection, it isn't real fire but a Fire Flower emitting a shining light. An old wooden door NORTH of you blocks your way NORTH. It does not budge. You see a way WEST of you.\n",
-	"fireflower", item_12,false,NULL,NULL,NULL,NULL,NULL,NULL);
-
-	
+	NULL,item_11,false,NULL,NULL,NULL,NULL,NULL,NULL); //creates room 1-1, the starting room
 
 	chamber* room_12 = new_room("ROOM 1-2. The room's walls are smokey and dark. Metal sconces are chained to the walls and their flames lick up at crumbling ceiling.\nOne of the flames behaves differently from the others and doesn't burn you as you approach.\nOn further inspection, it isn't real fire but a Fire Flower emitting a shining light. An old wooden door NORTH of you blocks your way NORTH. It does not budge. You see a way WEST of you.\n",
-	"Fire Flower", item_12,false,NULL,NULL,NULL,NULL,NULL,NULL);
-
+	"Fire Flower", item_12,false,NULL,NULL,NULL,NULL,NULL,NULL); //creates room 1-2
 
 	chamber* room_13 = new_room("ROOM 1-3. The first thing you notice are stones and dust falling from the rooms ceiling.\nThe ceiling in this room is falling apart and would most likely give if you even touched it, but you are too short as you are.\nThe room's floor gives way to dirt instead of stone. You see a way SOUTH and WEST of you.\n",
-	"Mushroom", NULL,true,NULL,NULL,NULL,NULL,NULL,NULL);
+	"Mushroom", NULL,true,NULL,NULL,NULL,NULL,NULL,NULL); //creates room 1-3
 
 	chamber* room_14 = new_room("ROOM 1-4. Intricate stone stairs spiral up into the ceiling. Small specks of dust and dirt float down from the floor above.\nYou can climb the stairs UP and you see a way EAST of you.\n",
-	NULL, NULL,false,NULL,NULL,NULL,NULL,NULL,NULL);
+	NULL, NULL,false,NULL,NULL,NULL,NULL,NULL,NULL); //creates room 1-4
 
 	chamber* room_21 = new_room("ROOM 2-1. The floor is littered with discarded koopa shells and their putrid smell makes your eyes water.\nSome of the koopa shells still have skelettons inside them while other skeletons are chained to the wall.\nYou can't help but feel bad. There are stairs that lead DOWN and a way to continue SOUTH of you.\n",
-	NULL, NULL,false,NULL,NULL,NULL,NULL,NULL,NULL);
+	NULL, NULL,false,NULL,NULL,NULL,NULL,NULL,NULL); //creates room 2-1
 
 	chamber* room_22 = new_room("ROOM 2-2. The room is empty except for crumbled and ripped up paper all over the floor. No writing desk is in the room.\nYou pick up a paper and inspect it, but it is indecipherable plans.\nThe poorly drawn plumber and dinosaur remind you of someone. You see a way NORTH and EAST.\n",
-	NULL, NULL,false,NULL,NULL,NULL,NULL,NULL,NULL);
+	NULL, NULL,false,NULL,NULL,NULL,NULL,NULL,NULL); //creates room 2-2
 
 	chamber* room_23 = new_room("ROOM 2-3. Blocks cover the ground and there is suspicious mound of them in the corner of the room.\nThe blocks cannot be lifted and there's no way to break them with a Mushroom since you can't get under them.\nThe room is a dead-end. You see only the way you came from WEST of you.\n",
-	"Koopa Shell", NULL,false,NULL,NULL,NULL,NULL,NULL,NULL);
+	"Koopa Shell", NULL,false,NULL,NULL,NULL,NULL,NULL,NULL); //creates room 2-3
 
 	chamber* room_24 = new_room("ROOM 2-4. The room is huge and completely black. You're standing on the edge of a bridge that is chained above a lake of lava.\nA huge green lizard with a spiked shell looms on the other side of the bridge.\nFire pours from his unhinged mouth. Behind him is an axe as well as where the bridge is precariously chained.\n",
-	"Axe", NULL,true,NULL,NULL,NULL,NULL,NULL,NULL);
+	"Axe", NULL,true,NULL,NULL,NULL,NULL,NULL,NULL); //creates room 2-4
+ 
+	set_east(room_11, room_12); //connects room 1-1 and 1-2
 
-	set_east(room_11,room_12);
+	set_north(room_12, room_13); //connects room 1-2 with 1-3
+	set_west(room_12, room_11); //connects room 1-2 with 1-1
 
-	set_north(room_12,room_13);
-	set_west(room_12,room_11);
+	set_south(room_13, room_12); //connects room 1-3 with 1-2
+	set_up(room_13, room_24); //connects room 1-3 with 2-4
+	set_west(room_13, room_14); //connects room 1-3 with 1-4
 
-	set_south(room_13,room_12);
-	set_up(room_13,room_24);
-	set_west(room_13,room_14);
+	set_up(room_14, room_21); //connects room 1-4 with 2-1
+	set_west(room_14, room_13); //connects room 1-4 with 1-3
+ 
+	set_south(room_21, room_22); //connects room 2-1 with 2-2
+	set_down(room_21, room_14); //connects room 2-1 with 1-4
 
-	set_up(room_14,room_21);
-	set_west(room_14,room_13);
+	set_north(room_22, room_21); //connects room 2-2 with 2-1
+	set_east(room_22, room_23); //connects room 2-2 with 2-3
 
-	set_south(room_21,room_22);
-	set_down(room_21,room_14);
-
-	set_north(room_22,room_21);
-	set_east(room_22,room_23);
-
-	set_west(room_23,room_22);
-
-	//Item*item_12 = item("","",item("Fire Flower", "a Fire Flower", NULL));
-
-	room_11->item = item("","",item("koopashell", "a Koopa Shell", NULL));
-
-	//room_21->item_list = item("","",item("Koopa Shell", "a Koopa Shell", NULL));
-	//room_24->item_list = item("","",item("Axe", "an Axe", NULL));
-	//room_12->item_list = item("","",item("Mushroom", "a Mushroom", NULL));
-
+	set_west(room_23, room_22); //connects room 2-3 with 2-2
 
 	return room_11;
-	
-
 }
-
-
-
-
-
-
