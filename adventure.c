276488013ae5bd321da59f067bf1cb6250ffb2b2
nhanfiction <47795547+nhanfiction@users.noreply.github.com>
2019-02-25 17:05:08 -0500

    Uses fgets, no one line speed run
    Need to fix use (using an item that doesn't exist causes crash)

--- adventure.c
+++ adventure.c
@@ -9,7 +9,6 @@
 struct user{
  struct Item* bag;
  chamber* stage;
- //char*current_stage;
 };
 
 typedef struct user user;
@@ -19,7 +18,6 @@ typedef struct user user;
 	struct user* mario = (struct user*)malloc(sizeof(struct user)); //allocates memory for the user
 	mario->bag = bag; //sets the bag
 	mario->stage = begin; //sets the room the user starts at
-	//mario->current_stage = current_stage;
 	return mario;
 }
 
@@ -31,22 +29,22 @@ void look(chamber*ptr){ //prints out the description of the room
 
 bool can_access(char*direction, user*ptr){ //checks if the direction is blocked or not
 	
-	if(strcmp("NORTH",direction)==0 && ptr->stage->north !=NULL && ptr->stage->north->isBlocked == false){
+	if(strncmp("NORTH", direction, 5)==0 && ptr->stage->north !=NULL && ptr->stage->north->isBlocked == false){
   		return true; //checks if north room is blocked
    	}
-  	else if(strcmp("SOUTH",direction)==0 && ptr->stage->south !=NULL && ptr->stage->south->isBlocked == false){
+  	else if(strncmp("SOUTH", direction, 5)==0 && ptr->stage->south !=NULL && ptr->stage->south->isBlocked == false){
   		return true; //checks if south room is blocked
   	}
-  	else if(strcmp("EAST",direction)==0 && ptr->stage->east !=NULL && ptr->stage->east->isBlocked == false){
+  	else if(strncmp("EAST", direction, 4)==0 && ptr->stage->east !=NULL && ptr->stage->east->isBlocked == false){
   		return true; //checks if east room is blocked
   	}
-  	else if(strcmp("WEST",direction)==0 && ptr->stage->west !=NULL && ptr->stage->west->isBlocked == false){
+  	else if(strncmp("WEST", direction, 4)==0 && ptr->stage->west !=NULL && ptr->stage->west->isBlocked == false){
   		return true; //checks if west room is blocked
   	}
-  	else if(strcmp("UP",direction)==0 && ptr->stage->up !=NULL && ptr->stage->up->isBlocked == false){
+  	else if(strncmp("UP", direction, 2)==0 && ptr->stage->up !=NULL && ptr->stage->up->isBlocked == false){
   		return true; //checks if above room is blocked
   	}
- 	else if(strcmp("DOWN",direction)==0 && ptr->stage->down !=NULL && ptr->stage->down->isBlocked == false){
+ 	else if(strncmp("DOWN", direction, 4)==0 && ptr->stage->down !=NULL && ptr->stage->down->isBlocked == false){
   		return true; //checks if lower room is blocked
   	} 
 
@@ -56,32 +54,32 @@ bool can_access(char*direction, user*ptr){ //checks if the direction is blocked 
 
 
 void go(user* ptr, char* direction){  //changes the room user is in
-	if(strcmp("NORTH", direction)==0){ //goes north
+	if(strncmp("NORTH", direction, 5)==0){ //goes north
   		ptr->stage = ptr->stage->north; 
   		printf("You moved NORTH\n\n"); 
   		
    	}
-  	else if(strcmp("SOUTH", direction)==0){ //goes south
+  	else if(strncmp("SOUTH", direction, 5)==0){ //goes south
   		ptr->stage = ptr->stage->south;
   		printf("You moved SOUTH\n\n");
   		
   	}
-  	else if(strcmp("EAST", direction)==0){ //goes east
+  	else if(strncmp("EAST", direction, 4)==0){ //goes east
   		ptr->stage = ptr->stage->east;
   		printf("You moved EAST\n\n");
   		
   	}
-  	else if(strcmp("WEST", direction)==0){ //go west
+  	else if(strncmp("WEST", direction, 4)==0){ //go west
   		ptr->stage = ptr->stage->west;
   		printf("You moved WEST\n\n");
   		
   	}
-  	else if(strcmp("UP", direction)==0){ //go uo
+  	else if(strncmp("UP", direction, 2)==0){ //go uo
   		ptr->stage = ptr->stage->up;
   		printf("You moved UP\n\n");
   		
   	}
- 	else if(strcmp("DOWN", direction)==0){ //go down
+ 	else if(strncmp("DOWN", direction, 4)==0){ //go down
   		ptr->stage = ptr->stage->down;
   		printf("You moved DOWN\n\n");
   		
@@ -91,24 +89,24 @@ void go(user* ptr, char* direction){  //changes the room user is in
 
 
 void play_game(user* ptr){ //USER API
-	char* input1 = (char*)malloc(sizeof(char*)); //allocate memory to store first input string
-	char* input2 = (char*)malloc(sizeof(char*)); //allocate memory to store second input string
+	char input1[20]; //initialize strings for input
+	char input2[20]; //initialize strings for input
 
-	//char direction; //direction player chooses to go
 	bool end = false; //keeps the game going until player wins or quits
 
 	while(!end){
 		printf("What do you do? Type \"help\" for a list of commands\n");
-		scanf("%s", input1); //gets player's input
+		fgets(input1, 20, stdin); //stores input in input1
 		
-		if(strcmp("look", input1) == 0){ //look at room
+		if(strncmp("look", input1, 4) == 0){ //look at room
 			look(ptr->stage);
 		}
 
-		else if(strcmp("go", input1) == 0){ //go to another room
-			scanf("%s", input2);
+		else if(strncmp("go", input1, 2) == 0){ //go to another room
+			fgets(input2, 20, stdin); //stores input in input2
 			if(can_access(input2, ptr)){ //if room is not blocked
 				go(ptr, input2); //go to the room
+				continue;
 			}
 			else{
 				system("setterm -bold on");
@@ -117,22 +115,22 @@ void play_game(user* ptr){ //USER API
 			}
 		}
 
-		else if(strcmp("take", input1) == 0){ //take an item
+		else if(strncmp("take", input1, 4) == 0){ //take an item
 			printf("THE ITEMS AVAILABLE IN THIS ROOM\n");
 
 			if(ptr->stage->item->next != NULL){ //item list not empty
 				print_list(ptr->stage->item); //prints list of items
-				printf("what you want to take: "); 
-				scanf("%s", input2);
+				printf("What you want to take: "); 
+				fgets(input2, 20, stdin); //stores input in input2
 
-				while(!contain(input2, ptr->stage->item) && !strcmp(input2,"x")==0){ //rechoose if incorrect input given
+				while(!contain(input2, ptr->stage->item) && !strncmp("x", input2, 1) == 0){ //rechoose if incorrect input given
 					printf("ITEM IS NOT EXIST!!!!\n"); 
 					print_list(ptr->stage->item); //prints list again
 					printf("Choose again or x to stop: ");
-					scanf("%s", input2);
+					fgets(input2, 20, stdin); //stores input in input2
 				}
 				add_item(ptr->bag, item_take(input2, ptr->stage->item)); //takes item and adds it to the player's bag
-				printf("YOUR NEW INVENTORY");
+				printf("\nYOUR NEW INVENTORY\n");
 				print_list(ptr->bag); //prints out list of bag items
 				continue;
 				}
@@ -142,12 +140,14 @@ void play_game(user* ptr){ //USER API
 			}
 		}
 
-		else if(strcmp("use", input1) == 0){ //use an item from your bag
+		else if(strncmp("use", input1, 3) == 0){ //use an item from your bag
 			print_list(ptr->bag); //prints out bag
-			printf("WHAT YOU WANT TO USE:\n");
-			scanf("%s", input2);
-			if(strcmp(item_take(input2,ptr->bag)->name, ptr->stage->reqItem)==0){ //take item from bag
+			printf("\nWHAT YOU WANT TO USE: ");
+			fgets(input2, 20, stdin); //stores input in input2
+			if(strncmp(ptr->stage->reqItem, item_take(input2, ptr->bag)->name, strlen(ptr->stage->reqItem))==0){ //take item from bag
 				toggleBlocked(ptr->stage); //unblock next room
+				printf("The next room has been unblocked.\n");
+				continue;
 			}
 			else{
 				printf("You can't use that item here.\n");
@@ -155,24 +155,24 @@ void play_game(user* ptr){ //USER API
 			}
 		}
 
-		else if(strcmp("drop", input1) == 0){ //drop an item
+		else if(strncmp("drop", input1, 4) == 0){ //drop an item
 			print_list(ptr->bag); //prints out bag list
 			printf("what do you want to drop: ");
-			scanf("%s", input2);
+			fgets(input2, 20, stdin); //stores input in input2
 			Item*drop_ptr = item_take(input2, ptr->bag); //take item out of bag
 			printf("You dropped the %s\n", drop_ptr->name);
 			add_item(ptr->stage->item, drop_ptr); //adds item to the room's item list
 			
-			printf("YOUR INVENTORY:\n");
+			printf("\nYOUR INVENTORY:\n");
 			print_list(ptr->bag); 
 			continue;
 		}
 
-		else if(strcmp("help", input1) == 0){
+		else if(strncmp("help", input1, 4) == 0){
 			printf("Type \"go\" to go to another room.\nType \"look\" to look at your surroundings.\nType \"take\" to take an item in a room.\nType \"drop\" to drop something from your inventory.\nType \"use\" to use something in your inventory.\nType \"quit\" to quit the game. Your progress won't be saved.\n");
 			continue;
 		}
-		else if(strcmp("quit", input1) == 0){
+		else if(strncmp("quit", input1, 4) == 0){
 			end = true;
 		}
 		else{
