67a82359e875d2f0acaf628a2313ccadbbb521f6
nhanfiction <47795547+nhanfiction@users.noreply.github.com>
2019-02-26 20:54:45 -0500

    Update items.h

--- items.h
+++ items.h
@@ -10,14 +10,14 @@ struct Item{
 
 typedef struct Item Item;
 
-struct Item*item(char* name1, char* description1, struct Item*input);
+struct Item*item(char* name1, char* description1, struct Item*input); //creates new item
   
-char* item_name(struct Item*ptr);
+char* item_name(struct Item*ptr); //gets item name
  
-void print_list(struct Item*ptr);
+void print_list(struct Item*ptr); //prints the list of items in the room
 
-char* item_description(struct Item*ptr);
+char* item_description(struct Item*ptr); //gets item description
  
-struct Item* item_take(char*name,Item *head);
+struct Item* item_take(char*name,Item *head); //takes an item from a list of items
 
-void add_item(struct Item*bag, struct Item*new);
+void add_item(struct Item*bag, struct Item*new); //adds an item to another list
