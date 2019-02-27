38896196ff7d5f14cc4371c18981fc4699469eb4
Thisausername99 <39577021+Thisausername99@users.noreply.github.com>
2019-02-27 16:41:24 -0500

    Add files via upload

--- items.c
+++ items.c
@@ -3,27 +3,23 @@
 #include <string.h>
 
 
-char* item_name(Item* input){ //gets item name
-  return input->name;
-}
 
-char* item_description(Item* input){ //gets item description
-  return input->description;
-}
 
 void print_list(Item*head){ //prints list
   if(head->next == NULL){ //head is the dummy node, so next is first in list
-    printf("ROOM HAS NO ITEMS\n"); //room is empty if next thing is null
+    printf("BAG IS EMPTY\n"); //room is empty if next thing is null
     return;
   }
   struct Item*current = head->next;
-  int i = 1; //item number
+  int i = 1;
   while(current != NULL){ //traversing the item list
     printf("*Item %i is: %s \n*Description: %s \n", i, current->name, current->description);
     ++i; //increment the item number
     current = current->next;
   }
 }
+
+
 
 Item*item(char* name1, char* description1, struct Item* input){ //creates new item
   struct Item*temp = (struct Item*)malloc(sizeof(struct Item)); //allocates space for item
@@ -34,10 +30,11 @@ Item*item(char* name1, char* description1, struct Item* input){ //creates new it
   return temp;
 }
 
+
 Item* item_take(char*name, Item *head){ //take an item from the room
     Item* result = NULL; //pointer to the item we're taking
     Item* previous = NULL; //previous item in the list
-    previous = head; //head will become curr's previous node
+    previous = head; 
     Item* curr = head->next; // skip the dummy node
     while(curr != NULL){ //traversing item list
       if(strncmp(curr->name, name, strlen(curr->name)) == 0){ //item found
@@ -53,6 +50,7 @@ Item* item_take(char*name, Item *head){ //take an item from the room
     }
     return result;
 }
+
 
 void add_item(struct Item*bag, struct Item*new){ //adds item to player's bag
   if(bag->next == NULL){ //nothing else in list
