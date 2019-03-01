4425fb47d2c155afb825675811d6ded1443f651a
nhanfiction <47795547+nhanfiction@users.noreply.github.com>
2019-02-28 23:06:03 -0500

    Deleted unused things + added comments
    bool toggleBlocked(chamber* current);
    void room_desc(chamber*current);
    bool state_conquered(char*item,chamber* current);
    void setRoomItem(chamber*room, struct Item* item);
    bool takeRoomItem(chamber*room, struct Item* item);

--- rooms.h
+++ rooms.h
@@ -19,25 +19,25 @@ struct Room{
 typedef struct Room chamber;
 
 chamber * new_room(char* desc, char* tool, Item *item_list, bool blocked, 
-chamber *north, chamber *south,  chamber *east, chamber *west, chamber *up, chamber *down);
+chamber *north, chamber *south,  chamber *east, chamber *west, chamber *up, chamber *down); //creates a new room
 
 
-void display_room(chamber*room);
+void display_room(chamber*room); //prints room description
 
-void set_north(chamber*current,chamber*other);
+void set_north(chamber*current,chamber*other); //sets other room to current's north
 
-void set_south(chamber*current,chamber*other);
+void set_south(chamber*current,chamber*other); //sets other room to current's south
 
-void set_east(chamber*current,chamber*other);
+void set_east(chamber*current,chamber*other); //sets other room to current's east
 
-void set_west(chamber*current,chamber*other);
+void set_west(chamber*current,chamber*other); //sets other room to current's west
 
-void set_up(chamber*current,chamber*other);
+void set_up(chamber*current,chamber*other); //sets other room to current's up
 
-void set_down(chamber*current,chamber*other);
+void set_down(chamber*current,chamber*other); //sets other room to current's down
 
-bool contain(char*search,Item*curr_room);
+bool contain(char*search,Item*curr_room); //checks if an item is inside a room
 
-chamber* load_room();
+chamber* load_room(); //creates the rooms
 
-void useItem(char* item, chamber* ptr);
+void useItem(char* item, chamber* ptr); //uses the item, unblocks next room, and changes room description
